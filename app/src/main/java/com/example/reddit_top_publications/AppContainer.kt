8a8424c5c0eda28d2e5fc8f5801212b53028795a
OveraLL <ru111kravtsov@gmail.com>
2024-11-08 22:57:10 +0200

    basic auth with response

--- app/src/main/java/com/example/reddit_top_publications/AppContainer.kt
+++ app/src/main/java/com/example/reddit_top_publications/AppContainer.kt
@@ -1,7 +1,7 @@
 package com.example.reddit_top_publications
 
 import android.app.Application
-import com.example.reddit_top_publications.data.TokenAuthenticator
+import com.example.reddit_top_publications.data.TokenInterceptor
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -11,7 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class AppContainer : Application() {
     companion object {
-        /*private val gson = GsonBuilder().setLenient().create()*/
         private val retrofitBuilder: Retrofit.Builder =
             Retrofit.Builder()
                 .baseUrl("https://oauth.reddit.com/")
@@ -21,25 +20,13 @@ class AppContainer : Application() {
     private var retrofit: Retrofit = retrofitBuilder.build()
 
     private val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-        .authenticator(TokenAuthenticator())
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
+        .addInterceptor(TokenInterceptor())
+
 
     fun <T> createService(serviceClass: Class<T>): T {
-        /*if (token != null) {
-            httpClient.interceptors().clear()
-            httpClient.addInterceptor { chain ->
-                val original: Request = chain.request()
-                val request: Request = original.newBuilder()
-                    .header("Authorization", "Bearer $token")
-                    .build()
-                chain.proceed(request)
-            }
-            retrofitBuilder.client(httpClient.build())
-            retrofit = retrofitBuilder.build()
-        }
-        return retrofit.create(serviceClass)*/
         retrofit = retrofitBuilder.client(httpClient.build()).build()
         return retrofit.create(serviceClass)
     }
--- app/src/main/java/com/example/reddit_top_publications/data/TokenAuthenticator.kt
+++ app/src/main/java/com/example/reddit_top_publications/data/TokenAuthenticator.kt
@@ -5,21 +5,33 @@ import android.util.Log
 import com.example.reddit_top_publications.network.ApiService
 import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
+import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-class TokenAuthenticator : Authenticator {
-    override fun authenticate(route: Route?, response: Response): Request {
+class TokenInterceptor : Interceptor {
+    companion object {
+        private const val CREDENTIALS = "8Kjz4vJC7wkYvsXCkHqpQQ:R18ikmdNHQ0Xks-QE0Gkqdq3mCyTfQ"
+        private const val USERNAME = "user-user1233"
+        private const val PASSWORD = "zxcvbnm123"
+        private const val USER_AGENT = "PostmanRuntime/7.42.0"
+        private const val CONTENT_LENGTH = (CREDENTIALS.length + USERNAME.length + PASSWORD.length).toLong()
+        private const val HOST = "oauth.reddit.com"
+    }
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
         val newToken = runBlocking { refreshToken() }
-        Log.d("Token_Authenticator", "New token: $newToken")
-        return response.request.newBuilder()
+        Log.d("Token_Interceptor", "New token: $newToken")
+
+        val newRequest = originalRequest.newBuilder()
             .header("Authorization", "Bearer $newToken")
             .build()
 
+        return chain.proceed(newRequest)
     }
+
 
     suspend fun refreshToken(): String {
         val retrofit = Retrofit.Builder()
@@ -28,21 +40,16 @@ class TokenAuthenticator : Authenticator {
             .build()
 
         val apiService = retrofit.create(ApiService::class.java)
-        val credentials = "8Kjz4vJC7wkYvsXCkHqpQQ:R18ikmdNHQ0Xks-QE0Gkqdq3mCyTfQ"
-        val username = "user-user1233"
-        val password = "zxcvbnm123"
-        val userAgent = "PostmanRuntime/7.42.0"
-        val basicAuth = "Basic " + Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
-        val contentLength = (credentials.length + username.length + password.length).toLong()
-        val host = "oauth.reddit.com"
+
+        val basicAuth = "Basic " + Base64.encodeToString(CREDENTIALS.toByteArray(), Base64.NO_WRAP)
         val tokenResponse: TokenResponse = apiService.getAccessToken(
             authorization = basicAuth,
-            userAgent = userAgent,
-            contentLength = contentLength,
-            host = host,
+            userAgent = USER_AGENT,
+            contentLength = CONTENT_LENGTH,
+            host = HOST,
             grantType = "password",
-            username = username,
-            password = password
+            username = USERNAME,
+            password = PASSWORD
         )
         Log.d("TOKEN_RESPONSE", "$tokenResponse || $basicAuth" )
         return tokenResponse.access_token
